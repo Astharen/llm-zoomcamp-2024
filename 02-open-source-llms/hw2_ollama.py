b2182b1b23bd5fb23e8e8f70d5514fdc3b2b0e2e
AlmudenaZhou <almudena.zhou.rl@gmail.com>
2024-07-08 16:59:57 +0200

    Added homework module 2

---
+++ 02-open-source-llms/hw2_ollama.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+
+
+prompt = "What's the formula for energy"
+
+
+client = OpenAI(
+    base_url='http://localhost:11434/v1/',
+    api_key='ollama'
+)
+
+response = client.chat.completions.create(
+    model="gemma2b",
+    messages=[
+    {"role": "user", "content": prompt}
+  ],
+  temperature=0.0)
